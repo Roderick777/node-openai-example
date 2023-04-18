35f3375a24bd802a20d349b301595f0c225bf85c
Rodrigo González <rodrigoalexissss@gmail.com>
2023-04-17 22:50:20 -0400

    ajustes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 dotenv.config()
 
+const port = process.env.PORT || 8000
 const app = express()
 app.use(bodyParser.json())
 app.use(cors({ origin: '*' }))
@@ -11,6 +12,4 @@ app.use(cors({ origin: '*' }))
 app.get('/', async (req, res) => res.send('hola'))
 app.use('/api/openai/', require('./routes/openai.route'))
 
-app.listen(port, () =>
-  console.log(`go to: http://localhost:${process.env.PORT}`)
-)
+app.listen(port, () => console.log(`go to: http://localhost:${port}`))
