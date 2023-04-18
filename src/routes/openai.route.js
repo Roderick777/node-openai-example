3ef223f363ffe26f5d656e679426985f397a22b5
Rodrigo González <rodrigoalexissss@gmail.com>
2023-04-18 00:03:07 -0400

    FIX: Openai middleware error

--- src/routes/openai.route.js
+++ src/routes/openai.route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { OpenaiMiddleware } = require('../middleware/openai.middleware')
 const OpenaiController = require('../controllers/openai.controller')
 
 router.post('/', OpenaiMiddleware, OpenaiController.feedbackSuggest)
